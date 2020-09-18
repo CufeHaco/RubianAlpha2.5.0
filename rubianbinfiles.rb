90ed3ab74157fe544c3c4f6b0453d54bd875b9f7
CufeHaco <crazyrabbitking89@gmail.com>
2020-09-18 09:32:36 -0400

    Update rubianbinfiles.rb

--- rubianbinfiles.rb
+++ rubianbinfiles.rb
@@ -22,7 +22,7 @@ begin
 							         ]
 							         
 def check_bin_install
-check_bin_dir =Dir.exists?("#{Dir.home}/Desktop/Rubian/bin")
+check_bin_dir =Dir.exists?("#{Dir.home}/Rubian/bin")
 if check_bin_dir == false
 	    puts "rubian bin directory not found"
 	    @bin_installed = false
@@ -30,25 +30,25 @@ if check_bin_dir == false
 		@bin_installed = true
 		puts "Rubian bin directory found"
 		
-@file1  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[0]}")
-@file2  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[1]}")
-@file3  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[2]}")
-@file4  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[3]}")
-@file5  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[4]}")
-@file6  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[5]}")
-@file7  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[6]}")
-@file8  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[7]}")
-@file9  = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[8]}")
-@file10 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[9]}")
-@file11 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[10]}")
-@file12 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[11]}")
-@file13 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[12]}")
-@file14 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[13]}")
-@file15 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[14]}")
-@file16 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[15]}")
-@file17 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[16]}")
-@file18 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[17]}")
-@file19 = File.exists?("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[18]}")
+@file1  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[0]}")
+@file2  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[1]}")
+@file3  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[2]}")
+@file4  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[3]}")
+@file5  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[4]}")
+@file6  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[5]}")
+@file7  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[6]}")
+@file8  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[7]}")
+@file9  = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[8]}")
+@file10 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[9]}")
+@file11 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[10]}")
+@file12 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[11]}")
+@file13 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[12]}")
+@file14 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[13]}")
+@file15 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[14]}")
+@file16 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[15]}")
+@file17 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[16]}")
+@file18 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[17]}")
+@file19 = File.exists?("#{Dir.home}/Rubian/bin/#{@bin_file[18]}")
 if @file1||@file2||file3||file4||file5||@file6||@file7||@file8||@file9||@file10||@file11||@file12||@file13||@file14||@file15||file16||@file17||@file18||@file19 == true
 		puts "Rubian bin files found"
 		@bin_files_installed = true
@@ -58,34 +58,34 @@ end
 
 def uninstall_bin
 
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[0]}" )# if @file1 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[1]}" )# if @file2 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[2]}" )# if @file3 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[3]}" )# if @file4 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[4]}" )# if @file5 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[5]}" )# if @file6 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[6]}" )# if @file7 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[7]}" )# if @file8 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[8]}" )# if @file9 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[9]}" )# if @file10 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[10]}")# if @file11 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[11]}")# if @file12 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[12]}")# if @file13 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[13]}")# if @file14 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[14]}")# if @file15 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[15]}")# if @file16 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[16]}")# if @file17 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[17]}")# if @file18 ==true
-File.delete("#{Dir.home}/Desktop/Rubian/bin/#{@bin_file[18]}")# if @file19 ==true
-Dir.rmdir("#{Dir.home}/Desktop/Rubian/bin")	 
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[0]}" )# if @file1 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[1]}" )# if @file2 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[2]}" )# if @file3 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[3]}" )# if @file4 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[4]}" )# if @file5 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[5]}" )# if @file6 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[6]}" )# if @file7 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[7]}" )# if @file8 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[8]}" )# if @file9 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[9]}" )# if @file10 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[10]}")# if @file11 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[11]}")# if @file12 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[12]}")# if @file13 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[13]}")# if @file14 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[14]}")# if @file15 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[15]}")# if @file16 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[16]}")# if @file17 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[17]}")# if @file18 ==true
+File.delete("#{Dir.home}/Rubian/bin/#{@bin_file[18]}")# if @file19 ==true
+Dir.rmdir("#{Dir.home}/Rubian/bin")	 
 end
 		
 		
 def install_bin	
 
-Dir.mkdir("#{Dir.home}/Desktop/Rubian/bin")
+Dir.mkdir("#{Dir.home}/Rubian/bin")
 	
-Dir.chdir("#{Dir.home}/Desktop/Rubian/bin")
+Dir.chdir("#{Dir.home}/Rubian/bin")
 
 puts "Making #{@bin_file[0]}" ;sleep(1)
 file = File.new("#{@bin_file[0]}" , 'w')
