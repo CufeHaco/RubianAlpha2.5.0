b6c663c9b37de04d6b51b65a284f74ec12499c0c
CufeHaco <crazyrabbitking89@gmail.com>
2020-09-18 09:37:37 -0400

    Update rubiantmp.rb

--- rubiantmp.rb
+++ rubiantmp.rb
@@ -2,7 +2,7 @@ begin
 
 
 def check_tmp_install
-check_tmp_dir =Dir.exists?("#{Dir.home}/Desktop/Rubian/tmp")
+check_tmp_dir =Dir.exists?("#{Dir.home}/Rubian/tmp")
 if check_tmp_dir == false
 	    puts "Rubian tmp Directory not found"
 	    @tmp_installed = false
@@ -15,10 +15,10 @@ end
 
 
 def uninstall_tmp
-Dir.rmdir("#{Dir.home}/Desktop/Rubian/tmp")
+Dir.rmdir("#{Dir.home}/Rubian/tmp")
 end
 
 def install_tmp
-Dir.mkdir("#{Dir.home}/Desktop/Rubian/tmp")
+Dir.mkdir("#{Dir.home}/Rubian/tmp")
 end
 
